587135385d6ccdf106b7779a8032febbd278686b
Saverio Miroddi <saverio.pub2@gmail.com>
2022-08-23 13:37:58 +0200

    Add Clippy settings, and make minor, cosmetic cleanup

--- serdine/src/lib.rs
+++ serdine/src/lib.rs
@@ -1,4 +1,17 @@
-// Poor Man's Serde. Trivial pair of interfaces for de/serialization.
+#![deny(clippy::all)]
+#![allow(
+  // style includes the useful `redundant_closure`
+  clippy::assign_op_pattern,
+  clippy::collapsible_else_if,
+  clippy::collapsible_if,
+  clippy::comparison_chain,
+  clippy::derive_partial_eq_without_eq,
+  clippy::len_zero,
+  clippy::manual_range_contains,
+  clippy::new_without_default,
+  clippy::too_many_arguments,
+  clippy::type_complexity,
+)]
 
 mod builtin_types;
 mod deserialize;
--- serdine_derive/src/lib.rs
+++ serdine_derive/src/lib.rs
@@ -1,10 +1,24 @@
+#![deny(clippy::all)]
+#![allow(
+  // style includes the useful `redundant_closure`
+  clippy::assign_op_pattern,
+  clippy::collapsible_else_if,
+  clippy::collapsible_if,
+  clippy::comparison_chain,
+  clippy::derive_partial_eq_without_eq,
+  clippy::len_zero,
+  clippy::manual_range_contains,
+  clippy::new_without_default,
+  clippy::too_many_arguments,
+  clippy::type_complexity,
+)]
+
 mod bail;
 mod deserialize;
 mod field_data;
 
 use deserialize::impl_deserialize;
 use proc_macro::TokenStream;
-use syn::{self};
 
 #[proc_macro_derive(Deserialize, attributes(deserialize))]
 pub fn deserialize(input: TokenStream) -> TokenStream {
