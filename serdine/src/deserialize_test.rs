99db3cf3b926c72f2ca90bcbef2a35a90af26b9d
Saverio Miroddi <saverio.pub2@gmail.com>
2022-08-26 21:55:52 +0200

    Implement De/Serializable for `bool` data type

--- serdine/src/deserialize_test.rs
+++ serdine/src/deserialize_test.rs
@@ -13,6 +13,7 @@ pub struct MyNamedFieldsStruct {
     pub my_f32: f32,
     pub my_f64: f64,
     pub my_arr: [u16; 2],
+    pub my_bool: bool,
     #[deserialize = "deserialize_vec"]
     pub my_vec: Vec<u8>,
 }
@@ -32,6 +33,7 @@ fn test_deserialize_named_fields_struct() {
         0xC9, 0x3E, 0x7B, 0x44,
         0x0C, 0x07, 0x42, 0xB2, 0x80, 0x19, 0x24, 0x40,
         0x00, 0x01, 0x02, 0x03,
+        0xCA,
         0x04, 0x05, 0x06
     ];
 
@@ -42,6 +44,7 @@ fn test_deserialize_named_fields_struct() {
     assert_eq!(1004.981_f32, instance.my_f32);
     assert_eq!(10.04981_f64, instance.my_f64);
     assert_eq!([0x0100, 0x0302], instance.my_arr);
+    assert!(instance.my_bool);
     assert_eq!(vec![4, 5, 6], instance.my_vec);
 }
 
--- serdine/src/primitive_types.rs
+++ serdine/src/primitive_types.rs
@@ -22,6 +22,21 @@ impl_for_numeric!(
 
 impl_for_numeric!(Serialize, i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, f32, f64);
 
+impl Deserialize for bool {
+    fn deserialize<R: std::io::Read>(mut r: R) -> Self {
+        let mut buffer = [0; 1];
+        r.read_exact(&mut buffer).unwrap();
+        buffer[0] != 0
+    }
+}
+
+impl Serialize for bool {
+    fn serialize<W: std::io::Write>(&self, mut w: W) {
+        let buffer = [if *self { 1 } else { 0 }; 1];
+        w.write_all(&buffer).unwrap();
+    }
+}
+
 impl<T, const N: usize> Deserialize for [T; N]
 where
     T: Deserialize + Debug,
